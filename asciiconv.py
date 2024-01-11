47cf8152d0a56062d79d3ad6e673cbcacbc50227
Zeyaad <148540407+zeyaadplaysgames@users.noreply.github.com>
2024-01-11 20:18:54 +0400

    Update asciiconv.py
    fixed some bug idk

--- asciiconv.py
+++ asciiconv.py
@@ -5,14 +5,11 @@ def image_to_ascii(image_path, output_width=100, char_ratio=0.5):
     # Open the image
     img = Image.open(image_path)
 
-    # Resize the image
+    # Resize the image whith original aspect ratio
     aspect_ratio = img.height / img.width
     output_height = int(output_width * aspect_ratio)
     img = img.resize((output_width, output_height))
-
-    # Convert the image to grayscale
-    img = img.convert('L')
-
+    
     # Define ASCII characters based on intensity
     ascii_chars = "@%#*+=-:. "
 
@@ -25,13 +22,13 @@ def image_to_ascii(image_path, output_width=100, char_ratio=0.5):
         ascii_art += ascii_chars[char_index]
 
     # Break lines to match the output width
-    ascii_art = "\n".join([ascii_art[i:i+output_width] for i in range(0, le>
+    ascii_art = "\n".join([ascii_art[i:i+output_width] for i in range(0, len(ascii_art), output_width)])
 
     print(ascii_art)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 asciiconv.py <image_path>")
+        print("Usage: python3 program.py <image_path>")
     else:
         image_path = sys.argv[1]
         image_to_ascii(image_path)
